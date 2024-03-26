33963cbfe26d9be32f66bf62d6b6b272b05b9d05
Morgan Ogola <morganogola@gmail.com>
2024-03-26 18:22:36 +0300

    Added a create user mutation and crreate_post resolver. Defined the content and Post in blog

--- lib/blog_web/resolvers/content.ex
+++ lib/blog_web/resolvers/content.ex
@@ -6,5 +6,11 @@ defmodule BlogWeb.Resolvers.Content do
   def list_posts(_parent, _args, _resolution) do
     {:ok, Blog.Content.list_posts()}
   end
+  def create_post(_parent,args,%{context: %{current_user: user}}) do
+    Blog.Content.create_post(user,args)
+  end
+  def create_post(_parent,_args,_resolution) do
+    {:error, "Access Denied"}
+  end
 
 end
