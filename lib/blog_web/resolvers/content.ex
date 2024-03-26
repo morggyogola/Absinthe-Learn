9ddeb8ddb87c7231e40874e8fac3df970326aa9f
Morgan Ogola <morganogola@gmail.com>
2024-03-26 17:34:54 +0300

    Added list post to user struct. Added accounts, contact and user definitions

--- lib/blog_web/resolvers/content.ex
+++ lib/blog_web/resolvers/content.ex
@@ -1,5 +1,10 @@
 defmodule BlogWeb.Resolvers.Content do
+
+  def list_posts(%Blog.Accounts.User{} = author, args, _resolution) do
+    {:ok, Blog.Content.list_posts(author,args)}
+  end
   def list_posts(_parent, _args, _resolution) do
     {:ok, Blog.Content.list_posts()}
   end
+
 end
